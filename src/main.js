42deaa920dadd1f7a218ccffedc2f10b08421c3f
Hidde de Vries <hidde@hiddedevries.nl>
2019-12-12 16:10:33 +0100

    Format with prettier
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.getElementById('conformance-tool')
+  target: document.getElementById("conformance-tool"),
 });
 
 export default app;
