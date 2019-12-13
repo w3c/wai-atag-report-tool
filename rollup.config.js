6d9733a901a28acaadf01f6e7907d18f8f84f050
Hidde de Vries <hidde@hiddedevries.nl>
2019-12-13 11:54:08 +0100

    Use serve plugin so that routes go to index
    Also reformat according to Prettier
    
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,19 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import svelte from "rollup-plugin-svelte";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import serve from "rollup-plugin-serve";
+import { terser } from "rollup-plugin-terser";
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
+	input: "src/main.js",
 	output: {
 		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
+		format: "iife",
+		name: "app",
+		file: "public/build/bundle.js",
 	},
 	plugins: [
 		svelte({
@@ -21,8 +22,8 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
-				css.write('public/build/bundle.css');
-			}
+				css.write("public/build/bundle.css");
+			},
 		}),
 
 		// If you have external dependencies installed from
@@ -32,40 +33,29 @@ export default {
 		// https://github.com/rollup/rollup-plugin-commonjs
 		resolve({
 			browser: true,
-			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
+			dedupe: importee =>
+				importee === "svelte" || importee.startsWith("svelte/"),
 		}),
 		commonjs(),
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
-		!production && serve(),
+		!production &&
+			serve({
+				contentBase: "public",
+				historyApiFallback: true,
+			}),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production &&
+			livereload({
+				watch: "public",
+			}),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
 	],
-	watch: {
-		clearScreen: false
-	}
 };
 
-function serve() {
-	let started = false;
-
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
-
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
-}
