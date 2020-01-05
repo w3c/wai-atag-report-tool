f931671413d0c689676ae13b5dee7cd3b98fcb2d
Eric Eggert <mail@yatil.net>
2020-01-05 20:03:54 +0100

    Fix Guidelines typo and making 1 to 4 & 4 to 8 more readable
    ;-)
    
    Signed-off-by: Eric Eggert <mail@yatil.net>

--- src/routes/Start.svelte
+++ src/routes/Start.svelte
@@ -8,8 +8,8 @@
 <p>This process will take you through 8 steps, the <strong>Principles</strong>. Within these, there are <strong>Guidelines</strong>, which break down into <strong>Success Criteria</strong>.</p>
 <p>For each Success Criterion, you can select whether the authoring tool you are evaluation meets it or not.</p>
 <h2>Editor experience vs output</h2>
-<p>Guidelines 1-4 are related to the tool itself: the back-end of your CMS, the <strong>editing experience</strong>. Can it be used by people with disabilities?</p>
-<p>Guideliens 4-8 are about the <strong>output</strong> of the tool: does it encourage content editors to create accessible content, and is accessible content creation possible at all?</p>
+<p>Guidelines 1 to 4 are related to the tool itself: the back-end of your CMS, the <strong>editing experience</strong>. Can it be used by people with disabilities?</p>
+<p>Guidelines 4 to 8 are about the <strong>output</strong> of the tool: does it encourage content editors to create accessible content, and is accessible content creation possible at all?</p>
 
 <Pager label="Previous/Next Step">
   <PagerLink to="/step/1" direction="next">Get started</PagerLink>
