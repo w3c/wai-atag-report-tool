9b3f1e811707c661cf667d600205b58f6c316621
Hidde de Vries <hidde@hiddedevries.nl>
2020-01-30 22:39:22 +0100

    Add oaf-svelte-routing to improve routing accessibility

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import App from "./App.svelte";
+import { globalHistory as history } from "svelte-routing/src/history";
+import { wrapHistory } from "oaf-svelte-routing";
+
+wrapHistory(history);
 
 const app = new App({
   target: document.getElementById("conformance-tool"),
