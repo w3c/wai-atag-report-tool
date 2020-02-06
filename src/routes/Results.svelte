ffd7eb7e816a65015b32405c3a7e2e751d21fe6f
Hidde de Vries <hidde@hiddedevries.nl>
2020-02-06 12:15:27 +0100

    Display evaluation in HTML at Results step (#20)

--- src/routes/Results.svelte
+++ src/routes/Results.svelte
@@ -2,13 +2,28 @@
   import { evaluation } from '../stores/evaluation.js';
   import NavLink from '../components/NavLink.svelte';
 
-  $: jsonDownload = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify($evaluation))}`;
+  $: jsonDownload = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(Object.values($evaluation)))}`;
 </script>
 
 <h2>Results</h2>
-<p>Thanks for using this tool.</p>
-<ul>
-  <li><a href={jsonDownload} download="data.json">Download JSON</a></li>
-</ul>
+<p>Thanks for using this tool. Your evaluation is displayed in full below. You can also <a href={jsonDownload} download="data.json">Download JSON</a>.</p>
 
+<table>
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Result</th>
+			<th>Observations</th>
+		</tr>
+	</thead>
+	<tbody>
+	{#each Object.values($evaluation) as result}
+	<tr>
+		<td>{result.handle}</td>
+		<td>{result.result ? result.result : 'No result'}</td>
+		<td>{result.observations ? result.observations : 'No observations'}</td>
+	</tr>
+	{/each}
+	</tbody>
+</table>
 <NavLink to="/step/1">Start over</NavLink>
