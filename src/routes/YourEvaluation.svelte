5ba63e87e84920cc8b167f4cf03c4228e1213287
Hidde de Vries <hidde@hiddedevries.nl>
2020-04-03 14:39:39 +0200

    Fix double IDs

--- src/routes/YourEvaluation.svelte
+++ src/routes/YourEvaluation.svelte
@@ -31,13 +31,13 @@
 <h2>About you</h2>
 
 <div class="field">
-  <label for="evaluation-meta-name">Your name</label>
-  <input type="text" bind:value={$evaluation["meta"]["evaluatorName"]["value"]} id="evaluation-meta-name"on:change={() =>  evaluation.updateCache($evaluation)} />
+  <label for="evaluation-meta-your-name">Your name</label>
+  <input type="text" bind:value={$evaluation["meta"]["evaluatorName"]["value"]} id="evaluation-meta-your-name"on:change={() =>  evaluation.updateCache($evaluation)} />
 </div>
 
 <div class="field">
-  <label for="evaluation-meta-name">Your organisation</label>
-  <input type="text" bind:value={$evaluation["meta"]["evaluatorOrg"]["value"]} id="evaluation-meta-name"on:change={() =>  evaluation.updateCache($evaluation)} />
+  <label for="evaluation-meta-org-name">Your organisation</label>
+  <input type="text" bind:value={$evaluation["meta"]["evaluatorOrg"]["value"]} id="evaluation-meta-org-name"on:change={() =>  evaluation.updateCache($evaluation)} />
 </div>
 
 <Pager label="Previous/Next Step">
