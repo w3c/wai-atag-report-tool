5612239c9b1929d51ae07d62ea76abbd0d4b9f5f
Hidde de Vries <hidde@hiddedevries.nl>
2020-05-07 15:58:56 +0200

    Revert icon, use Markdown supported (#89)

--- src/components/EvaluationObservation.svelte
+++ src/components/EvaluationObservation.svelte
@@ -23,11 +23,9 @@
       Observations
       <span class="visuallyhidden">for {num}</span>
     </label>
-    <Link to={'/#markdown'}>
-      <span class="visuallyhidden">Markdown supported</span>
-      <svg aria-hidden="true" focusable="false" width="24" height="24" viewBox="0 0 24 24"><path style="fill: currentColor;" d="M22.269 19.385H1.731a1.73 1.73 0 0 1-1.73-1.73V6.345a1.73 1.73 0 0 1 1.73-1.73h20.538a1.73 1.73 0 0 1 1.73 1.73v11.308a1.73 1.73 0 0 1-1.73 1.731zm-16.5-3.462v-4.5l2.308 2.885 2.307-2.885v4.5h2.308V8.078h-2.308l-2.307 2.885-2.308-2.885H3.461v7.847zM21.231 12h-2.308V8.077h-2.307V12h-2.308l3.461 4.039z"/></svg>
-    </Link>
     <span class="observation__meta">
+      <Link to={'/#markdown'}>Markdown supported</Link>
+      <span aria-hidden="true"> ・ </span>
       <Link to={`/report#criterion-${normalisedCriterionId}`}>View in report<span class="visuallyhidden"> for {normalisedCriterionId}</span></Link>
     </span>
   </div>
