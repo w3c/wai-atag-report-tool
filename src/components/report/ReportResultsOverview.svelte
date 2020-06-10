121b076a8cc1e9c05e7664e4b3d3c37c9eea9f04
Hidde de Vries <hidde@hiddedevries.nl>
2020-06-10 11:39:12 +0200

    Show results for all categories always, not just those with evaluated items (#132)

--- src/components/report/ReportResultsOverview.svelte
+++ src/components/report/ReportResultsOverview.svelte
@@ -1,56 +1,31 @@
 <script>
-  import LinkToSC from '../LinkToSC.svelte';
+  import ReportResultsOverviewResult from "./ReportResultsOverviewResult.svelte";
   import { evaluation } from "../../stores/evaluation.js";
-  import { getLinkToSC } from '../../utils/getLinkToSC.js';
-  import { getEvaluatedItems } from '../../utils/getEvaluatedItems.js';
+  import { getEvaluatedItems } from "../../utils/getEvaluatedItems.js";
+
+  const resultCategories = [
+    "Passed",
+    "Failed",
+    "Cannot tell",
+    "Not applicable",
+    "Not checked",
+  ];
+
+  function getItemsFromCategory(category) {
+    return items.filter(item => item.result === category);
+  }
 
   $: items = getEvaluatedItems($evaluation);
-  $: itemsByResult = {
-    passed: items.filter(item => item.result === 'Passed'),
-    failed: items.filter(item => item.result === 'Failed'),
-    cannottell: items.filter(item => item.result === 'Cannot tell'),
-    na: items.filter(item => item.result === 'Not applicable'),
-    notchecked: items.filter(item => item.result === 'Not checked')
-  };
 </script>
 
 <h3>Overview</h3>
 
-<p>This report has results for {items.length} out of {Object.values($evaluation.evaluationData).length} Success Criteria.</p>
+<p>This report has results for {items.length} Success Criteria.</p>
 
 <ul>
-  {#if itemsByResult.passed.length > 0 }
-  <li>Passed ({itemsByResult.passed.length}):
-    {#each itemsByResult.passed as item}
-    <LinkToSC href={getLinkToSC(item.num)}>{item.num}</LinkToSC>
-    {/each}
-  </li>
-  {/if}
-  {#if itemsByResult.failed.length > 0 }
-  <li>Failed ({itemsByResult.failed.length}):
-    {#each itemsByResult.failed as item}
-    <LinkToSC href={getLinkToSC(item.num)}>{item.num}</LinkToSC>
-    {/each}
-  </li>
-  {/if}
-  {#if itemsByResult.cannottell.length > 0 }
-  <li>Cannot tell ({itemsByResult.cannottell.length}):
-    {#each itemsByResult.cannottell as item}
-    <LinkToSC href={getLinkToSC(item.num)}>{item.num}</LinkToSC>
-    {/each}
-  </li>
-  {/if}
-  {#if itemsByResult.na.length > 0 }
-  <li>Not applicable:
-    {#each itemsByResult.na as item}
-    <LinkToSC href={getLinkToSC(item.num)}>{item.num}</LinkToSC>
-    {/each}
-  </li>
-  {/if}
-  {#if itemsByResult.notchecked.length > 0 }
-  <li>Not checked ({itemsByResult.notchecked.length}):
-    {#each itemsByResult.notchecked as item}
-    <LinkToSC href={getLinkToSC(item.num)}>{item.num}</LinkToSC>
-    {/each}
-  {/if}
+  {#each resultCategories as category}
+    <ReportResultsOverviewResult
+      label={category}
+      items={getItemsFromCategory(category)} />
+  {/each}
 </ul>
---
+++ src/components/report/ReportResultsOverviewResult.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { evaluation } from "../../stores/evaluation.js";
+  import LinkToSC from "../LinkToSC.svelte";
+  import { getLinkToSC } from "../../utils/getLinkToSC.js";
+
+  export let label;
+  export let items = "none";
+</script>
+
+<li>
+  {label} ({items.length} reported)
+  {#each items as item}
+    <LinkToSC href={getLinkToSC(item.num)}>{item.num}</LinkToSC>
+  {/each}
+</li>
