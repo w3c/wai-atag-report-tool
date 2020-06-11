2763ed701eda6c07ff0ca525760ed102ce5201c9
Hidde de Vries <hidde@hiddedevries.nl>
2020-06-11 20:38:34 +0200

    Remove chevron (#60)

--- src/components/EvaluationInfoPrincipleDetail.svelte
+++ src/components/EvaluationInfoPrincipleDetail.svelte
@@ -47,19 +47,6 @@
     <span class="principle">
       <a href={getLinkToPrinciple(principle)}>
         <span>{principle}</span>
-        <svg
-          style="vertical-align: middle;"
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round">
-          <polyline points="9 18 15 12 9 6" />
-        </svg>
       </a>
     </span>
     <span class="progress" style="margin-left: auto;">{done} of {total}</span>
