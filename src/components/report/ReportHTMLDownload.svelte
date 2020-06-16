d38f685416b5c568bb38334e34f1652e498a2219
Hidde de Vries <hidde@hiddedevries.nl>
2020-06-16 12:15:13 +0200

    Add tool name to report, tweak report title (fixes #147)

--- src/components/report/ReportHTMLDownload.svelte
+++ src/components/report/ReportHTMLDownload.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from "svelte";
+  import { evaluation } from "../../stores/evaluation.js";
 
   import ReportHeader from "./ReportHeader.svelte";
   import ReportResultsTable from "./ReportResultsTable.svelte";
@@ -7,12 +8,16 @@
   import { cleanUp } from "../../utils/cleanUpReportHTML.js";
   import { createHTMLDownload } from "../../utils/createHTMLDownload.js";
 
-  const title = "Authoring Tool Accessibility Report";
-  let htmlDownload, htmlDownloadTemplate;
+  const title = "Accessibility Report";
+  let htmlDownload, htmlDownloadTemplate, nameProvided;
 
   onMount(() => {
     htmlDownload = createHTMLDownload(htmlDownloadTemplate, title);
   });
+  $: nameProvided =
+    $evaluation["meta"] &&
+    $evaluation["meta"]["name"] &&
+    $evaluation["meta"]["name"]["value"];
 </script>
 
 <a href={htmlDownload} download="report.html" class="button">
@@ -20,7 +25,10 @@
 </a>
 
 <div hidden use:cleanUp bind:this={htmlDownloadTemplate}>
-  <h1>{title}</h1>
+  <h1>
+    {title}
+    {#if nameProvided}for {$evaluation['meta']['name']['value']}{/if}
+  </h1>
   <ReportHeader />
   <ReportResultsTable />
 </div>
