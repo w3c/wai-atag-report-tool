dc4c52b9856da12b14ca173145c353845a4b8d5d
Hidde de Vries <hidde@hiddedevries.nl>
2020-06-17 10:17:08 +0200

    Remove log

--- src/utils/inConformanceTarget.js
+++ src/utils/inConformanceTarget.js
@@ -1,7 +1,6 @@
 export function inConformanceTarget(item, evaluation) {
   const conformanceTarget = evaluation.meta.conformanceTarget.value.length;
 
-  console.log(conformanceTarget);
   // check if this is a Success Criterion that supports multiple levels
   if (item.level !== item.evaluatedLevel) {
     return (
