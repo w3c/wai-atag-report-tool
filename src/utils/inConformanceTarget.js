6c52c1f38c2be6a5efdd7a0de30aa10718de1352
Hidde de Vries <hidde@hiddedevries.nl>
2020-06-19 14:29:19 +0200

    Double check if there is evaluated level

--- src/utils/inConformanceTarget.js
+++ src/utils/inConformanceTarget.js
@@ -2,7 +2,7 @@ export function inConformanceTarget(item, evaluation) {
   const conformanceTarget = evaluation.meta.conformanceTarget.value.length;
 
   // check if this is a Success Criterion that supports multiple levels
-  if (item.level !== item.evaluatedLevel) {
+  if (item.evaluatedLevel && item.level !== item.evaluatedLevel) {
     return (
       item.evaluatedLevel.replace("Level ", "").length <= conformanceTarget
     );
