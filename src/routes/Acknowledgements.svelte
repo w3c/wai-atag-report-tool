ed4129500a189241d778e410e79491e2d4a4951c
shawn_slh <shawn@w3.org>
2020-06-23 11:54:22 -0500

    minor suggestions

--- src/routes/Acknowledgements.svelte
+++ src/routes/Acknowledgements.svelte
@@ -15,9 +15,7 @@
 <Header>Acknowledgements</Header>
 
 <p>
-  Developed by the Education and Outreach Working Group (
-  <a href="http://www.w3.org/WAI/EO/">EOWG</a>
-  ) with support from the
+  The ATAG Report Tool was developed by the Education and Outreach Working Group (<a href="http://www.w3.org/WAI/EO/">EOWG</a>) with support from the
   <a href="https://www.w3.org/WAI/about/projects/wai-guide/">WAI-Guide</a>
   project funded by the European Commission (EC) under the Horizon 2020 program
   (Grant Agreement 822245).
@@ -25,17 +23,17 @@
 
 <h2>Project Leadership</h2>
 <ul>
-  <li>Developer and Editor: Hidde de Vries, W3C WAI</li>
-  <li>Project Lead: Shadi Abou-Zahra, W3C WAI</li>
-  <li>EOWG Co-Chairs: Brent Bakken, Pearson and Sharron Rush, Knowbility</li>
-  <li>EOWG Staff Contact: Shawn Lawton Henry, W3C WAI</li>
+  <li><strong>Developer and Editor:</strong> Hidde de Vries, W3C WAI</li>
+  <li><strong>Project Lead:</strong> Shadi Abou-Zahra, W3C WAI</li>
+  <li><strong>EOWG Co-Chairs:</strong> Brent Bakken, Pearson and Sharron Rush, Knowbility</li>
+  <li><strong>EOWG Staff Contact:</strong> Shawn Lawton Henry, W3C WAI</li>
 </ul>
 
 <h2>Major Contributors</h2>
 <ul>
   <li>Shadi Abou-Zahra, W3C WAI</li>
   <li>Shawn Lawton Henry, W3C WAI</li>
-  <li>Eric Eggert, Knowbility/W3C</li>
+  <li>Eric Eggert, Knowbility and W3C</li>
   <li>Kevin White, The Scottish Government</li>
   <li>Daniel Montalvo, W3C WAI</li>
 </ul>
@@ -45,10 +43,10 @@
 <ul>
   <li>Brent Bakken, Pearson plc</li>
   <li>Helen Burge, Deque Systems, Inc</li>
-  <li>Sylvie Duchateau , Invited Expert</li>
+  <li>Sylvie Duchateau</li>
   <li>Laura Keen, Library of Congress</li>
   <li>Howard Kramer, University of Colorado Boulder</li>
-  <li>Vicki Menezes Miller, Invited Expert</li>
+  <li>Vicki Menezes Miller</li>
   <li>Estella Oncins, Universitat Autònoma de Barcelona</li>
   <li>Mark Palmer, The Scottish Government</li>
 </ul>
