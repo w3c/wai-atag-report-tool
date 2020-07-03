f0795267dfcfbca2e1acdad8d524757b6a652bb7
Hidde de Vries <hidde@hiddedevries.nl>
2020-07-03 10:47:56 +0200

    Make link to principle env specific (#176)

--- src/utils/getLinkToPrinciple.js
+++ src/utils/getLinkToPrinciple.js
@@ -1,8 +1,10 @@
+import vars from "../../config/__buildEnv__.json";
+
 export function getLinkToPrinciple(num) {
   const principle =
     num.substring(0, 1) === "A"
       ? num.substring(2, 3)
       : parseInt(num.substring(2, 3), 10) + 4;
 
-  return `/principle/${principle}`;
+  return `${vars.pathPrefix}/principle/${principle}`;
 }
